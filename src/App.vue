1d7ceecd2e8d2eaa6714f8a80eb4347f2f50c617
Kymaeda <kyo.m.720@gmail.com>
2022-09-13 09:45:36 +0900

    setup functions with Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { ref, computed } from 'vue';
+const START_FROM = 10;
+export default {
+  // NOTE: OptionalAPI syntax
+  // data() {
+  //   return { counter: 0 }
+  // }
+  // computed: {
+  //   muplited() { return this.counter * 10 }
+  // }
+  setup() {
+    const counter = ref(72);
+    const muplited = computed(() => counter.value * 10);
+    const increment = () => counter.value++;
+    // NOTE: setup() needs to return values
+    return { counter, muplited, START_FROM, increment };
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>Hello! {{ counter }}</div>
+  <button @click="increment">Increase</button>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
