3bfa3f97b63ea3d5479c469d22032973cee657e7
Kymaeda <kyo.m.720@gmail.com>
2022-09-14 09:26:29 +0900

    reative properties with `ref` and `reactive`

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import { ref, computed } from 'vue';
+import { ref, computed, reactive } from 'vue';
 const START_FROM = 10;
 export default {
   // NOTE: OptionalAPI syntax
@@ -10,11 +10,26 @@ export default {
   //   muplited() { return this.counter * 10 }
   // }
   setup() {
+    // NOTE: `ref` works with Object and Primitive(wrapped with Object inside `ref` func), and call `reactive` internally
     const counter = ref(72);
+    // NOTE: `reactive` works with only Object. Primitive is not acceptable
+    const tokyo = reactive({ counter: 10 });
+
+    const ra = ref([1, 2, 3]);
+    const replaceArray = () => (ra.value = [2, 4, 6]);
+
     const muplited = computed(() => counter.value * 10);
     const increment = () => counter.value++;
     // NOTE: setup() needs to return values
-    return { counter, muplited, START_FROM, increment };
+    return {
+      START_FROM,
+      counter,
+      tokyo,
+      ra,
+      replaceArray,
+      muplited,
+      increment,
+    };
   },
 };
 </script>
@@ -22,4 +37,9 @@ export default {
 <template>
   <div>Hello! {{ counter }}</div>
   <button @click="increment">Increase</button>
+
+  <button @click="replaceArray">Replace</button>
+  <div>{{ ra }}</div>
+
+  <div>{{ tokyo.counter }}</div>
 </template>
