cc5933a90854bbed0a96507afbf45801bf2aaec2
Kymaeda <kyo.m.720@gmail.com>
2022-09-20 09:52:09 +0900

    switch to script setup
    - be able to use normal script tag and setup script at the same time

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
 import { ref, computed, reactive, watch, onMounted } from 'vue';
 import Money from './Money.vue';
 // import { useStore } from 'vuex';
@@ -6,57 +6,50 @@ import { useRate } from './composable/useRate';
 
 let timeout = null;
 
-export default {
-  components: { Money },
-  setup() {
-    // const store = useStore();
-    const currentBalance = ref(0);
-    // const inUSD = computed(() => currentBalance.value * store.state.rate);
+// const store = useStore();
+const currentBalance = ref(0);
+// const inUSD = computed(() => currentBalance.value * store.state.rate);
 
-    const { rate, setRate } = useRate();
-    const inUSD = computed(() => currentBalance.value * rate.value);
+const { rate, setRate } = useRate();
+const inUSD = computed(() => currentBalance.value * rate.value);
 
-    const sessionCounter = ref(0);
-    const history = ref([]);
-    const exchangeRecords = reactive({
-      highestBalance: null,
-    });
+const sessionCounter = ref(0);
+const history = ref([]);
+const exchangeRecords = reactive({
+  highestBalance: null,
+});
 
-    // NOTE: do not `.value`
-    watch(currentBalance, (newValue, _oldValue) => {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        sessionCounter.value++;
-        history.value.push(newValue);
+// NOTE: do not `.value`
+watch(currentBalance, (newValue, _oldValue) => {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    sessionCounter.value++;
+    history.value.push(newValue);
 
-        if (currentBalance.value > exchangeRecords.highestBalance) {
-          exchangeRecords.highestBalance = currentBalance.value;
-        }
-      }, 500);
-    });
-    // NOTE: to watch array, need `.value`
-    watch(history.value, () => console.log('new history added...'));
-    // NOTE: A watch source can only be a getter/effect function, a ref, a reactive object, or an array of these types
-    // `() => exchangeRecords.highestBalance` works as getter function
-    watch(
-      () => exchangeRecords.highestBalance,
-      () => console.log('exhange record has changed...')
-    );
+    if (currentBalance.value > exchangeRecords.highestBalance) {
+      exchangeRecords.highestBalance = currentBalance.value;
+    }
+  }, 500);
+});
+// NOTE: to watch array, need `.value`
+watch(history.value, () => console.log('new history added...'));
+// NOTE: A watch source can only be a getter/effect function, a ref, a reactive object, or an array of these types
+// `() => exchangeRecords.highestBalance` works as getter function
+watch(
+  () => exchangeRecords.highestBalance,
+  () => console.log('exhange record has changed...')
+);
 
-    onMounted(() =>
-      setInterval(
-        () => (setRate([1.13, 1.14, 1.15][Math.floor(Math.random() * 3)])),
-        // store.commit(
-        //   'setRate',
-        //   [1.13, 1.14, 1.15][Math.floor(Math.random() * 3)]
-        // ),
-        1000
-      )
-    );
-
-    return { currentBalance, inUSD, sessionCounter, history, exchangeRecords };
-  },
-};
+onMounted(() =>
+  setInterval(
+    () => (setRate([1.13, 1.14, 1.15][Math.floor(Math.random() * 3)])),
+    // store.commit(
+    //   'setRate',
+    //   [1.13, 1.14, 1.15][Math.floor(Math.random() * 3)]
+    // ),
+    1000
+  )
+);
 </script>
 
 <template>
--- src/Money.vue
+++ src/Money.vue
@@ -1,33 +1,35 @@
 <script>
+console.log('loaded normal script tag ...');
+</script>
+
+<script setup>
 import { computed, toRefs } from 'vue';
 // import { useStore } from 'vuex';
 import { useRate } from './composable/useRate';
+
+console.log('loaded setup script tag ...');
 
 const CURRENCY = {
   EUR: 'euro',
   US: 'us',
 };
-export default {
-  props: ['currency', 'balance'],
-  emits: ['updated'],
 
-  setup(props, { emit }) {
-    console.log(props);
-    // const store = useStore();
-    const { rate } = useRate();
+// NOTE: defineProps
+const props = defineProps(['currency', 'balance']);
+// NOTE: defineEmits
+const emit = defineEmits(['updated']);
 
-    // const { balance } = ref(props.balance)
-    const { balance } = toRefs(props);
+// const store = useStore();
+const { rate } = useRate();
 
-    // reactive data needs to be accessed with `.value` property
-    const formatted = computed(() => Number(balance.value).toFixed(2));
-    // const rate = computed(() => store.state.rate);
+// const { balance } = ref(props.balance)
+const { balance } = toRefs(props);
 
-    const btnClicked = () => emit('updated', 100);
+// reactive data needs to be accessed with `.value` property
+const formatted = computed(() => Number(balance.value).toFixed(2));
+// const rate = computed(() => store.state.rate);
 
-    return { CURRENCY, formatted, btnClicked, rate };
-  },
-};
+const btnClicked = () => emit('updated', 100);
 </script>
 
 <template>
